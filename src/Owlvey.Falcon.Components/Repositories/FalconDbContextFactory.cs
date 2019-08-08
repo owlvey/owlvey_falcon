bfde622762ad281fd2ab7e006b9a6edec38655f9
chenton147 <chenton147@hotmail.com>
2019-08-08 00:06:52 +0000

    add migrations

---
+++ src/Owlvey.Falcon.Components/Repositories/FalconDbContextFactory.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Owlvey.Falcon.Repositories
+{
+    public class FalconDbContextFactory : IDesignTimeDbContextFactory<FalconDbContext>
+    {
+        public FalconDbContext Create(string connectionString = null)
+        {
+            return CreateDbContext(new string[] { connectionString });
+        }
+
+        public FalconDbContext CreateDbContext(string[] args)
+        {
+            
+            var builder = new DbContextOptionsBuilder<FalconDbContext>();
+
+            var connectionString = "FalconDb.db";
+
+            if (String.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new InvalidOperationException("Could not find a connection string named 'DefaultConnection'.");
+            }
+
+            if (args.Length > 0)
+            {
+                connectionString = args[0];
+            }
+            
+            builder.UseSqlite(connectionString);
+
+            return new FalconDbContext(builder.Options);
+        }
+    }
+}
