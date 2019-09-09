8ee7076aa7bb773324605ae13233864208d6c121
Gregory Valderrama Vilca <gcvalderrama@hotmail.com>
2019-09-08 22:57:30 -0500

    add

--- src/Owlvey.Falcon.Components/Migrations/20190909012111_InitialCreate.Designer.cs
+++ src/Owlvey.Falcon.Components/Migrations/20190909012111_InitialCreate.Designer.cs
@@ -9,7 +9,7 @@ using Owlvey.Falcon.Repositories;
 namespace Owlvey.Falcon.Migrations
 {
     [DbContext(typeof(FalconDbContext))]
-    [Migration("20190909010057_InitialCreate")]
+    [Migration("20190909012111_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
