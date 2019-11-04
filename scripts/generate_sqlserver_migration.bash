b66e6be6ab8e1f807e6d8544aa345a3c06288c9d
Gregory Valderrama <gcvalderrama@hotmail.com>
2019-11-04 13:32:51 -0500

    add

--- scripts/generate_sqlserver_migration.bash
+++ scripts/generate_sqlserver_migration.bash
@@ -1,4 +1,16 @@
 export ASPNETCORE_ENVIRONMENT=prod
+
+rm -rf ./../src/Owlvey.Falcon.Components/Migrations
+
+rm ./../src/Owlvey.Falcon.Api/FalconDb.db
+
+pushd ./../src/Owlvey.Falcon.Components
+
+dotnet ef migrations add InitialCreate
+
+popd
+
+rm ./../artifactory/falcondb.sql
 dotnet build ./../Owlvey.Falcon.sln -v q
 cd ./../src/Owlvey.Falcon.Components
 dotnet ef migrations script -c FalconDbContext -o ../../artifactory/falcondb.sql --verbose
