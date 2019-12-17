4c8bc8a953d039920a8ea0c279f9d7ee3d11381b
Gregory Valderrama <gcvalderrama@hotmail.com>
2019-12-17 14:40:16 -0500

    add

--- scripts/generate_sqlserver_migration.bash
+++ scripts/generate_sqlserver_migration.bash
@@ -6,10 +6,9 @@ rm -rf ./../src/Owlvey.Falcon.Components/Migrations
 
 rm ./../src/Owlvey.Falcon.Api/FalconDb.db
 
-pushd ./../src/Owlvey.Falcon.Components
-
 dotnet build ./../Owlvey.Falcon.sln -v q
 
+pushd ./../src/Owlvey.Falcon.Components
 
 echo dotnet ef migrations add initial create 
 
