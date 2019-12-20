4217035790fe18cf6bec180b79244de327800fb6
Gregory Valderrama Vilca <gcvalderrama@hotmail.com>
2019-12-20 00:40:00 -0500

    add

--- src/Owlvey.Falcon.Components/Models/OperationDashboardRp.cs
+++ src/Owlvey.Falcon.Components/Models/OperationDashboardRp.cs
@@ -12,16 +12,12 @@ namespace Owlvey.Falcon.Models
         public StatsValue FeaturesStats { get; set; }
         public decimal FeaturesCoverage { get; set; }
         public StatsValue ServicesStats { get; set; }
-
         public int SLOFail { get; set; }
         public decimal SLOProportion { get; set; }
-
         public List<FeatureGetListRp> Features { get; set; } = new List<FeatureGetListRp>();
         public List<SourceGetListRp> Sources { get; set; } = new List<SourceGetListRp>();
         public List<ServiceGetListRp> Services { get; set; } = new List<ServiceGetListRp>();
-
         public List<SquadGetListRp> Squads { get; set; } = new List<SquadGetListRp>();
-
 
         [JsonConverter(typeof(JsonIDictionaryConverter<int, List<int>>))]
         public IDictionary<int, List<int>> ServiceMaps { get; set; } = new Dictionary<int, List<int>>();
