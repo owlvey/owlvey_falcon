7778647c1e7d8d03e080fd01b7ba44daa79b9750
Gregory Valderrama <gcvalderrama@hotmail.com>
2020-01-07 10:09:37 -0500

    add

---
+++ src/Owlvey.Falcon.Core/Entities/ClueEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Owlvey.Falcon.Core.Entities
+{
+    public class ClueEntity : BaseEntity
+    {
+        public SourceEntity Source { get; set; } 
+        public int SourceId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        
+        public int Value { get; set; } = 0;
+
+        public ClueEntity()
+        {
+
+        }
+
+    }
+}
