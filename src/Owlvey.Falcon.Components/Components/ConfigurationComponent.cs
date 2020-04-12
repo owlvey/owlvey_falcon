658b964cd3afae1c200be25798b055143c41d3b4
Gregory Valderrama Vilca <gcvalderrama@hotmail.com>
2020-04-12 11:38:31 -0500

    add

--- src/Owlvey.Falcon.Components/Components/ConfigurationComponent.cs
+++ src/Owlvey.Falcon.Components/Components/ConfigurationComponent.cs
@@ -6,7 +6,7 @@ namespace Owlvey.Falcon.Components
 {
     public class ConfigurationComponent
     {
-        public int DefaultRetryAttempts { get; set; } = 3;
-        public TimeSpan DefaultPauseBetweenFails { get; set; } = TimeSpan.FromSeconds(5);
+        public int DefaultRetryAttempts { get; set; } = 5;
+        public TimeSpan DefaultPauseBetweenFails { get; set; } = TimeSpan.FromSeconds(4);
     }
 }
