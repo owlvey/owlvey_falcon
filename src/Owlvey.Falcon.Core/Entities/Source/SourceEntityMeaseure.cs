42b13a6682765b8259aeb1dd4c82d5181996cd7e
Gregory Valderrama Vilca <gcvalderrama@hotmail.com>
2020-05-13 14:33:08 -0500

    add

--- src/Owlvey.Falcon.Core/Entities/Source/SourceEntityMeaseure.cs
+++ src/Owlvey.Falcon.Core/Entities/Source/SourceEntityMeaseure.cs
@@ -10,7 +10,7 @@ namespace Owlvey.Falcon.Core.Entities
     public partial class SourceEntity
     {
         public ProportionMeasureValue MeasureProportion(DatePeriodValue period = null)
-        {            
+        {
             IEnumerable<SourceItemEntity> data;
             if (period != null)
             {
@@ -20,14 +20,22 @@ namespace Owlvey.Falcon.Core.Entities
             {
                 data = this.SourceItems;
             }
-
             if (data.Count() == 0)
             {
                 return new ProportionMeasureValue(1, false);
             }
 
-            var proportion = QualityUtils.CalculateAverage(data.Select(c => c.Proportion));            
-            return new ProportionMeasureValue(proportion);
+            if (this.Kind == SourceKindEnum.Interaction) {
+                var temp = data.OfType<InteractionSourceItemEntity>().ToList();
+                var good = temp.Select(c => c.Good).Sum();
+                var total = temp.Select(c => c.Total).Sum();
+                var proportion = QualityUtils.CalculateProportion(total, good);
+                return new ProportionMeasureValue(proportion);
+            }
+            else {
+                var proportion = QualityUtils.CalculateAverage(data.Select(c => c.Proportion));
+                return new ProportionMeasureValue(proportion);
+            }            
         }
     }
 }
